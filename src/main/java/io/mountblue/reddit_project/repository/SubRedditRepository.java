914a25d1cf36f92a4057999bcaca9767c05e86c7
Sachin Sharma <sachinsharma81494@gmail.com>
2024-09-18 14:48:14 +0530

    subreddit repository created

---
+++ src/main/java/io/mountblue/reddit_project/repository/SubRedditRepository.java
@@ -0,0 +1,18 @@
+package io.mountblue.reddit_project.repository;
+
+import io.mountblue.reddit_project.model.SubReddit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SubRedditRepository extends JpaRepository<SubReddit,Long> {
+    @Query("select distinct name from SubReddit ")
+    List<String> findAllByName();
+
+    @Query("select s from SubReddit s where s.name= :name")
+    SubReddit findByName(@Param("name") String name);
+}
