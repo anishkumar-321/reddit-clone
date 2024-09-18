fe52c71b8716ce3577eee077422e6b99ae6d9266
Sachin Sharma <sachinsharma81494@gmail.com>
2024-09-18 12:05:47 +0530

    sachin commit

--- src/main/java/io/mountblue/reddit_project/RedditProjectApplication.java
+++ src/main/java/io/mountblue/reddit_project/RedditProjectApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RedditProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RedditProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(RedditProjectApplication.class, args);
+        System.out.println("Hello world!");
+    }
 
 }
