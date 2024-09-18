9c7a933fa1e6984f9744ffb4b4ce612aae07a0ef
sdas <sasmithadaspiki@gmail.com>
2024-09-18 12:30:07 +0530

    commit sdas

--- src/main/java/io/mountblue/reddit_project/RedditProjectApplication.java
+++ src/main/java/io/mountblue/reddit_project/RedditProjectApplication.java
@@ -9,7 +9,7 @@ public class RedditProjectApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(RedditProjectApplication.class, args);
-        System.out.println("I am Anish!");
+        System.out.println("I am sdas!");
     }
 
 }
